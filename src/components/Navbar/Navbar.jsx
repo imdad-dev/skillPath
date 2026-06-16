82e40e743a7942a2b26c96e6f8574cce25efd6fe
unknown <imdadulhoquekk@gmail.com>
2026-06-17 03:56:04 +0530

    feat : Create modern  Naber UI

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,62 @@
+import './Navbar.css';
+
 const Navbar = ()=>{
-    return <div>NavBar</div>
+    return (
+        <nav className="navbar">
+    <div className="logo">
+  🚀 SkillPath <span>AI</span>
+</div>
+
+  <div className="search-box">
+  <span className="search-icon">🔍</span>
+
+  <input
+    className="search-input"
+    type="text"
+    placeholder="Search skills, courses & growth paths..."
+  />
+</div>
+
+    <ul className="nav-links">
+        {/* Explore Learn Progress More */}
+        <li><a href="/explore">Explore</a></li>
+        <li><a href="/learn">Learn</a></li>
+        <li><a href="/progress">Progress</a></li>
+
+        {/* more is dropdown section of More ▼
+├── AI Tutor
+├── Mock Interview
+├── ATS Checker
+├── Resume Builder
+├── Job Search
+├── Notes
+└── Community */}
+
+     <li className="dropdown">
+          <span>More ▼</span>
+
+          <ul className="dropdown-content">
+              {/* items */}
+                <li><a href="/ai-tutor">AI Tutor</a></li>   
+                <li><a href="/mock-interview">Mock Interview</a></li>
+                <li><a href="/ats-checker">ATS Checker</a></li>
+                <li><a href="/resume-builder">Resume Builder</a></li>
+                <li><a href="/job-search">Job Search</a></li>
+                <li><a href="/notes">Notes</a></li>
+                <li><a href="/community">Community</a></li>
+          </ul>
+      </li>
+       
+    </ul>
+
+    <div className="nav-actions">
+        {/* Theme button + Get Started */}
+        
+         <button className="theme-toggle">🌙</button>
+        <button className="get-started">Get Started</button>
+    </div>
+</nav>
+    );
 }
 
 export default Navbar;
